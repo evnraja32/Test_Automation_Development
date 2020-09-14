7c2b783bd1400bad3e7613a6232fb4f8b823f804
evnraja32 <evnraja32@gmail.com>
2020-09-14 17:50:43 +0530

    Findind duplicate char from a char array

---
+++ src/test/java/practice_programs/CharArrayFindDuplicateCharInArray.java
@@ -0,0 +1,25 @@
+package practice_programs;
+
+import org.testng.annotations.Test;
+
+public class CharArrayFindDuplicateCharInArray {
+	
+	@Test
+	public void testMethod1() {
+		
+	}
+
+	@Test
+	public void testMethod2() {
+		
+	}
+	
+	private char processAndFindDuplicateChar(char[] inputArray) {
+		char result;
+		
+		
+		
+		
+		return result;
+	}
+}
