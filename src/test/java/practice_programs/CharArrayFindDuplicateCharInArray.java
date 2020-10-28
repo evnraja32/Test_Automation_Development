34eb92e010a90bf7ad46902c888c853cb7837fb3
evnraja32 <evnraja32@gmail.com>
2020-10-28 17:48:45 +0530

    Sign-off from office laptop
    Signed-off-by: evnraja32 <evnraja32@gmail.com>

--- src/test/java/practice_programs/CharArrayFindDuplicateCharInArray.java
+++ src/test/java/practice_programs/CharArrayFindDuplicateCharInArray.java
@@ -1,25 +1,41 @@
 package practice_programs;
 
+import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.Test;
 
 public class CharArrayFindDuplicateCharInArray {
-	
+
 	@Test
 	public void testMethod1() {
-		
+		char[] inputArray = { 'c', 'e', '7', 'q', 'c', 'p' };
+		char expected = 'c';
+		char actual = processAndFindDuplicateChar(inputArray);
+		assertEquals(actual, expected);
 	}
 
 	@Test
 	public void testMethod2() {
-		
+		char[] inputArray = { 'c', 'e', '7', 'q', 'c', 'p', 'a', 'p' };
+		char expected = 'c';
+		char actual = processAndFindDuplicateChar(inputArray);
+		assertEquals(actual, expected);
 	}
-	
+
 	private char processAndFindDuplicateChar(char[] inputArray) {
-		char result;
-		
-		
-		
-		
+		char result = 0;
+		char tempChar;
+		int len = 0, maxLen = 0;
+
+		String input = new String(inputArray);
+		while (input.length() != 0) {
+			tempChar = input.charAt(0);
+			len = (input.length() - (input = input.replaceAll(tempChar + "", "")).length());
+			if (len > maxLen) {
+				result = tempChar;
+				maxLen = len;
+			}
+		}
 		return result;
 	}
 }
